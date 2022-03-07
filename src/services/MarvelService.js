c786411c4b20c0181d700dd4466773b814b1eba1
Cheryl <coderedxd03@gmail.com>
2022-03-07 11:09:43 +0200

    heroku

---
+++ src/services/MarvelService.js
@@ -0,0 +1,39 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=c15b13541379dfee304ffe0fb17ca669';
+    _baseOffSet = 210;
+    
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status:${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async (offset = this._baseOffSet) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name, 
+            description: char.description ? char.description.slice(0, 210) : "No info about character",
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items   
+        }
+    }
+}
+
+export default MarvelService;
